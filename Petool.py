8f56bc5f248d82a34a0b6d205fba2d5259f4d0fb
dilip223 <63009147+dilip223@users.noreply.github.com>
2020-04-03 11:13:02 -0600

    Add files via upload

--- Petool.py
+++ Petool.py
@@ -2,6 +2,8 @@ import pefile
 import os
 import datetime
 import peutils
+import re
+
 directory_in_str = str('C:/Users/pandi/PycharmProjects/PEtool/executables') #the directory file of all the executables
 
 for root, dir, files in os.walk(directory_in_str):
@@ -21,64 +23,85 @@ for root, dir, files in os.walk(directory_in_str):
         print ('The Imports are listed below as: ')
         for entry in pe.DIRECTORY_ENTRY_IMPORT: #this code determines the directory imports of the executable or dll
             print entry.dll
-            for imp in entry.imports:
-                print '\t', hex(imp.address), imp.name
 
+           # for imp in entry.imports:
+            #    print '\t', hex(imp.address), imp.name #uncomment if you want to see imports within import entries
 
+        try:
+            for exp in pe.DIRECTORY_ENTRY_EXPORT.symbols:
+                print hex(pe.OPTIONAL_HEADER.ImageBase + exp.address), exp.name, exp.ordinal
+        except:
+            pass
 
 #This section is used to do a size check of the main sections. A flag is returned corresponding to section name if the
 #difference is high between raw and virtual data
-        diff=[] #create a list to store raw and virtual size difference
-        size_flag=[]
+        diff = [] #create a list to store raw and virtual size difference
+        size_flag = []
+        upx_ind = 0
+        print 'The sections are:'
         for section in pe.sections:
             print (section.Name, hex(section.VirtualAddress),
-            section.Misc_VirtualSize, section.SizeOfRawData )
-            diff_temp=abs(section.Misc_VirtualSize-section.SizeOfRawData)
-            if diff_temp>1000:
+                   section.Misc_VirtualSize, section.SizeOfRawData)
+            if section.Name.find('UPX')==0:
+                upx_ind = 1
+            diff_temp = abs(section.Misc_VirtualSize-section.SizeOfRawData)
+            if diff_temp > 1000:
                 size_flag.append(section.Name)
         print 'The size flags raised are',size_flag
+        if upx_ind == 1:
+            print 'The file has been packed with UPX'
 
+        #STRINGS SECTION
         strings = []
-        rt_string_idx= [entry.id for entry in pe.DIRECTORY_ENTRY_RESOURCE.entries].index(pefile.RESOURCE_TYPE['RT_STRING'])
-        rt_string_directory = pe.DIRECTORY_ENTRY_RESOURCE.entries[rt_string_idx]
-        for entry in rt_string_directory.directory.entries:
+        rsc_idx = [entry.id for entry in pe.DIRECTORY_ENTRY_RESOURCE.entries]#.index(pefile.RESOURCE_TYPE['RT_STRING'])
+        print 'The indexes for Resource Directory are' , rsc_idx
+        for i,rt_indxs in enumerate(rsc_idx):
+            string_directory = pe.DIRECTORY_ENTRY_RESOURCE.entries[i]
+            for entry in string_directory.directory.entries:
+                # Get the RVA of the string data and
+                # size of the string data
+                data_rva = entry.directory.entries[0].data.struct.OffsetToData
+                size = entry.directory.entries[0].data.struct.Size
+                #print 'Directory entry at RVA', hex(data_rva), 'of size', hex(size)
 
-            # Get the RVA of the string data and
-            # size of the string data
-            #
-            data_rva = entry.directory.entries[0].data.struct.OffsetToData
-            size = entry.directory.entries[0].data.struct.Size
-            print 'Directory entry at RVA', hex(data_rva), 'of size', hex(size)
-
-            # Retrieve the actual data and start processing the strings
-            #
-            data = pe.get_memory_mapped_image()[data_rva:data_rva + size]
-            offset = 0
-            while True:
-                # Exit once there's no more data to read
-                if offset >= size:
-                    break
-                # Fetch the length of the unicode string
+                # Retrieve the actual data and start processing the strings
                 #
-                ustr_length = pe.get_word_from_data(data[offset:offset + 2], 0)
-                offset += 2
+                data = pe.get_memory_mapped_image()[data_rva:data_rva + size]
+                offset = 0
+                while True:
+                    # Exit once there's no more data to read
+                    if offset >= size:
+                        break
+                    # Fetch the length of the unicode string
+                    ustr_length = pe.get_word_from_data(data[offset:offset + 2], 0)
+                    offset += 2
+                    # If the string is empty, skip it
+                    if ustr_length == 0:
+                        continue
+                    # Get the Unicode string
+                    ustr = pe.get_string_u_at_rva(data_rva + offset, max_length=ustr_length)
+                    offset += ustr_length * 2
+                    strings.append(ustr)
+                    #print 'String of length', ustr_length, 'at offset', offset
 
-                # If the string is empty, skip it
-                if ustr_length == 0:
-                    continue
-
-                # Get the Unicode string
-                #
-                ustr = pe.get_string_u_at_rva(data_rva + offset, max_length=ustr_length)
-                offset += ustr_length * 2
-                strings.append(ustr)
-                #print 'String of length', ustr_length, 'at offset', offset
-
-        print rt_string_directory.directory.entries
-
+        path_str = []
+        while ("" in strings):
+            strings.remove("")
+        print 'The strings extracted are'
         for i,stri in enumerate(strings):
             print i,stri
+            count = 0
+            urls = re.findall('(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+', stri)
+            while count <= len(stri):
+                count += 1
+                new_string = stri.split(stri[:count], 1)
+                if os.path.exists(new_string[1]):
+                    if stri.find(" \ ")==0:
+                        path_str.append(stri)
+                    break
+        print 'The OS path strings extracted were = ', path_str
+        print 'The URL extracted from strings were = ', urls
+
         signatures = peutils.SignatureDatabase('C:/Users/pandi/PycharmProjects/PEtool/UserDB.txt')
         matches = signatures.match(pe, ep_only=True)
-        print matches
-
+        print 'The match found with the existing signature database (i.e the file packer signature) were = ', matches
